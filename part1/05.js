89248d15d434b358c293a05884227427a94bfc18
Rajesh-Devo <jerryjd899@gmail.com>
2023-01-31 14:54:13 +0530

    type of

--- part1/05.js
+++ part1/05.js
@@ -1,5 +1,10 @@
 let firstName = "jerry";
-console.log(firstName.toUpperCase());
+console.log(firstName.toUpperCase());  //toUpperCase
 
 let secondName = "TOMMY";
-console.log(secondName.toLowerCase());
+console.log(secondName.toLowerCase());   //toLowerCase
+
+
+let thirdName = "harshit";
+let newString = thirdName.slice(1-5);
+console.log(newString);
---
+++ part1/06.js
@@ -0,0 +1,10 @@
+// type of 
+
+let age = 22;
+let firstName = "jerry";
+console.log(typeof age);
+
+
+let age1 = 22;
+let firstName1 = "jerry";
+console.log(typeof firstName1);
