daa09adacf958668787fa5a08fb183d538a79ecf
Rajesh-Devo <jerryjd899@gmail.com>
2023-02-01 11:01:10 +0530

    compariosn operator

--- part1/06.js
+++ part1/06.js
@@ -8,3 +8,11 @@ console.log(typeof age);
 let age1 = 22;
 let firstName1 = "jerry";
 console.log(typeof firstName1);
+
+//convert number to string
+ age = age +  "";
+ console.log(typeof age);
+
+ //convert string to number
+ let mystr =  + "mystr";
+ console.log(typeof mystr);
---
+++ part1/07.js
@@ -0,0 +1,10 @@
+//comparison operator
+
+let num1 = "7";
+let num2 = 7;
+console.log(num1==num2);  //== operator
+
+
+let num3 = 8;
+let num4 = "8";
+console.log(num3===num4);  //=== operator 
